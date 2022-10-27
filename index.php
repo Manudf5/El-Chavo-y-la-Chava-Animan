c8196bf9c5bbeaa9bf273f14adb3284b24318fe0
valmr18 <116838743+valmr18@users.noreply.github.com>
2022-10-27 16:06:39 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>El Chavo y La Chava Animan</title>
+	<link  rel="icon"   href="imagenes/Logo.png" type="image/png" />
+	<link rel="stylesheet" href="index.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@1,300&display=swap" rel="stylesheet">
+</head>
+<body>
+	<main class="contenedor">
+		<div class="contenedor_logo">
+			<img src="imagenes/Logo.png" class="Logo">
+		</div>
+		<div class="contenedor_nombre">
+			<img src="imagenes/Nombre.png" class="Nombre">
+		</div>
+		<div class="contenedor_vector1">
+			<img src="imagenes/Vector1.png" class="Vector1">
+		</div>
+		<div class="contenedor_vector2">
+			<img src="imagenes/Vector2.png" class="Vector2">
+		</div>
+		<div class="contenedor_vector3">
+			<img src="imagenes/Vector3.png" class="Vector3">
+		</div>
+		<div class="contenedor_vector4">
+			<img src="imagenes/Vector4.png" class="Vector4">
+		</div>
+		<div class="contenedor_chava">
+			<img src="imagenes/Chava.png" class="Chava">
+		</div>
+		<div class="boxtrasero">
+			<div class= "boxtrasero_iniciarsesion">
+				<h2>¿Ya estás registrado?</h2>
+				<p>Haz click aquí para iniciar sesión</p>
+				<button id="boton_iniciarsesion">Iniciar sesión</button>
+			</div>
+			<div class="boxtrasero_registrarse">
+				<h2>¿Aún no estás registrado?</h2>
+				<p>Haz click aquí para registrarte</p>
+				<button id="boton_registrarse">Registrarse</button>
+			</div>
+		</div>
+		<div>
+			<form action="" class="formularioiniciar">
+				<h2>Iniciar sesión</h2>
+				<input type="text" name="cedula" placeholder="Cédula"><br>
+				<input type="password" name="contraseña" placeholder="Contraseña"><br>
+				<button>Acceder</button>
+			</form>
+		</div>	
+	</main>
+</body>
+</html>
