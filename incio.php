814561db561b55435ec980a35cae44c352433b07
valmr18 <116838743+valmr18@users.noreply.github.com>
2022-10-28 15:10:17 -0400

    Create incio.php

---
+++ incio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>El Chavo y La Chava Animan</title>
+	<link  rel="icon"   href="imagenes/Logo.png" type="image/png" />
+	<link rel="stylesheet" href="index.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+</head>
+<body>
+	<?php require 'diseño.php';?>
+	<div class="botones_opciones">
+		<h1>¡Bienvenido!</h1>
+		<button id="boton_cronograma">Cronograma semanal</button> <br>
+		<button id="boton_pagos">Consultar pagos</button>
+	</div>
+	<div class="contenedor_dhanna">
+			<img src="imagenes/Dhanna.png" class="Dhanna">
+		</div>
+</body>
+</html>
