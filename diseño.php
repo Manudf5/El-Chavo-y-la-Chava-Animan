e4c90b214da4716846a3ea98d066d59eb49e0866
valmr18 <116838743+valmr18@users.noreply.github.com>
2022-10-28 15:16:58 -0400

    Create diseño.php

---
+++ diseño.php
@@ -0,0 +1,21 @@
+<?php
+  echo '<main class="contenedor">';
+  echo '<div class="contenedor_logo">';
+  echo '<img src="imagenes/Logo.png" class="Logo">';
+  echo '</div>';
+  echo '<div class="contenedor_nombre">';
+  echo '<img src="imagenes/Nombre.png" class="Nombre">';
+  echo '</div>';
+  echo '<div class="contenedor_vector1">';
+  echo '<img src="imagenes/Vector1.png" class="Vector1">';
+  echo '</div>';
+  echo '<div class="contenedor_vector2">';
+  echo '<img src="imagenes/Vector2.png" class="Vector2">';
+  echo '</div>';
+  echo '<div class="contenedor_vector3">';
+  echo '<img src="imagenes/Vector3.png" class="Vector3">';
+  echo '</div>';
+  echo '<div class="contenedor_vector4">';
+  echo '<img src="imagenes/Vector4.png" class="Vector4">';
+  echo '</div>';
+?>  
